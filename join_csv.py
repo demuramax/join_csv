e5deef0d88b71e28b630101fcb0b416709ed26ee
Maxim <demuramax@gmail.com>
2022-04-26 11:56:35 +0300

    join csv

---
+++ join_csv.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import sys
+
+
+def join_csv(path1, path2, column_name, join_type='left'):
+    file1, file2 = pd.read_csv(path1), pd.read_csv(path2)
+    file1.set_index(column_name)
+    data = file1.join(file2.set_index(column_name), on=column_name, how=join_type)
+    return data
+    
+print(join_csv(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+
+print('Arguments list:', str(sys.argv), 'Number of arguments:', len(sys.argv))
